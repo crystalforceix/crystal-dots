b1ee403e97e2bf685c5c7b933360784954976185
crystalforceix <crystalforceix@gmail.com>
2025-07-03 21:17:06 +0700

    first checkpoint

---
+++ multi-scripts/testing-scripts/link-config.fish
@@ -0,0 +1,60 @@
+#!/usr/bin/env fish
+
+
+# Variables
+set -q _flag_noconfirm && set noconfirm '--noconfirm'
+set -q XDG_CONFIG_HOME && set -l config $XDG_CONFIG_HOME || set -l config $HOME/.config
+set -q XDG_STATE_HOME && set -l state $XDG_STATE_HOME || set -l state $HOME/.local/state
+
+# Helper funcs
+function _out -a colour text
+    set_color $colour
+    # Pass arguments other than text to echo
+    echo $argv[3..] -- ":: $text"
+    set_color normal
+end
+
+function log -a text
+    _out cyan $text $argv[2..]
+end
+
+function input -a text
+    _out blue $text $argv[2..]
+end
+
+# Comfirm Function
+function confirm-overwrite -a path
+    if test -e $path -o -L $path
+        # No prompt if noconfirm
+        if set -q noconfirm
+            input "$path already exists. Overwrite? [Y/n]"
+            log 'Removing...'
+            rm -rf $path
+        else
+            # Prompt user
+            read -l -p "input '$path already exists. Overwrite? [Y/n] ' -n" confirm || exit 1
+
+            if test "$confirm" = 'n' -o "$confirm" = 'N'
+                log 'Skipping...'
+                return 1
+            else
+                log 'Removing...'
+                rm -rf $path
+            end
+        end
+    end
+
+    return 0
+end
+
+# Niri
+if confirm-overwrite $config/niri
+    log 'Installing niri configs...'
+    ln -s ./../crystal-dots/.config/niri $config/niri
+end
+
+# Quickshell
+if confirm-overwrite $config/quickshell
+    log 'Installing quickshell configs...'
+    ln -s ./../crystal-dots/.config/quickshell $config/quickshell
+end
