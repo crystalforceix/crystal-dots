29236053b11cdadf7230eb57f6706793bb63bcfb
crystalforceix <crystalforceix@gmail.com>
2025-07-16 20:22:04 +0700

    add 2 packages in scripts

--- multi-scripts/testing-scripts/uninstall-crystal-dots.fish
+++ multi-scripts/testing-scripts/uninstall-crystal-dots.fish
@@ -22,7 +22,7 @@ cd ~/crystal-dots/multi-scripts/testing-scripts/ && ./unlink-config.fish
 
 # Remove dependacy packages of dotfiles
 log 'Removing dependacy packages of crystal-dots'
-sudo pacman -Rns maomaowm-git nwg-look rofi-wayland foot xdg-desktop-portal-wlr swaybg waybar wl-clip-persist cliphist wl-clipboard wlsunset xfce-polkit swaync pamixer lavalauncher-mao-git sway-audio-idle-inhibit-git swayidle dimland-git brightnessctl swayosd wlr-randr grim slurp satty swaylock-effects-git wlogout crystal-themes-fonts crystal-neovim
+sudo pacman -Rns wlr-dpms-git fastfetch maomaowm-git nwg-look rofi-wayland foot xdg-desktop-portal-wlr swaybg waybar wl-clip-persist cliphist wl-clipboard wlsunset xfce-polkit swaync pamixer lavalauncher-mao-git sway-audio-idle-inhibit-git swayidle dimland-git brightnessctl swayosd wlr-randr grim slurp satty swaylock-effects-git wlogout crystal-themes-fonts crystal-neovim
 
 # Remove crystal-dots folder
 log 'Removing crystal-dots folder'
